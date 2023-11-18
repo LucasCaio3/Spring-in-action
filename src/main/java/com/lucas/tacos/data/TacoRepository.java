5cff8e4c8172500ad805add56c6b3c0c3da99c2e
Lucas Caio Vargas <lucascaio9163@gmail.com>
2023-11-18 08:55:12 -0300

    TacoController antes de ser apagado

--- src/main/java/com/lucas/tacos/data/TacoRepository.java
+++ src/main/java/com/lucas/tacos/data/TacoRepository.java
@@ -1,9 +1,13 @@
 package com.lucas.tacos.data;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.lucas.tacos.Taco;
 
-public interface TacoRepository extends CrudRepository<Taco, Long>{
-	
+public interface TacoRepository extends CrudRepository<Taco, Long> {
+
+	Page<Taco> findAll(Pageable pageable);
+
 }
